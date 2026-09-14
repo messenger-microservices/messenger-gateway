f8bf2603c0696176b25b682ba5218e810e110139
pulsar <pulsarmn@yandex.ru>
2026-09-14 08:58:38 +0300

    feat: add MessengerGatewayApplication class-launcher

--- src/main/java/ru/pulsarmn/messenger/gateway/MessengerGatewayApplication.java
+++ src/main/java/ru/pulsarmn/messenger/gateway/MessengerGatewayApplication.java
@@ -1,9 +1,13 @@
 package ru.pulsarmn.messenger.gateway;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@SpringBootApplication
 public class MessengerGatewayApplication {
 
     static void main(String[] args) {
-
+        SpringApplication.run(MessengerGatewayApplication.class, args);
     }
 }
