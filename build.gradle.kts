f25180d342e1490768baf1962ab3a057c470b09c
pulsarmn <152626412+pulsarmn@users.noreply.github.com>
2026-09-14 09:16:53 +0300

    feat: add base API Gateway logic
    * chore: add Spring Cloud Gateway dependency
    
    * feat: add MessengerGatewayApplication class-launcher

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,15 @@
 plugins {
     java
+    alias(libs.plugins.spring.boot)
 }
 
 repositories {
     mavenCentral()
+}
+
+dependencies {
+    implementation(platform(libs.spring.boot.dependencies))
+    implementation(libs.spring.cloud.starter.gateway.server.webflux)
 }
 
 configure<JavaPluginExtension> {
